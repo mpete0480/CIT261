dc8fb0546b442bca79a64f4a3f6d0e4c162e7070
mpete0480 <mdpetersen1184@gmail.com>
2020-04-27 14:23:01 -0500

    Update main.js

--- week2/main.js
+++ week2/main.js
@@ -2,3 +2,19 @@ alert('Welcome to Quiz Ninja!');
 const question = "What is Superman's real name?";
 const answer = prompt(question);
 alert(`You answered ${answer}`);
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?", "Diana Prince"],
+    ["What is Batman's real name?", "Bruce Wayne"]
+];
+let score = 0; //start of the scoring;
+for(const [question,answer] of quiz){
+    const response = prompt(question);
+    if(response == answer){
+        alert('Correct!');
+        score++;
+    } else {
+        alert (`Wrong! The correct answer was ${answer}`);
+    }
+}
+alert(`Game Over, You scored ${score} point${score !==1 ? 's' : ''}`);
