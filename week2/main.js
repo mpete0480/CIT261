bab6692e28f46c4e6f05f63fbe21c79eb85f2d6d
mpete0480 <mdpetersen1184@gmail.com>
2020-04-28 16:36:14 -0500

    Update main.js

--- week2/main.js
+++ week2/main.js
@@ -1,20 +1,39 @@
 alert('Welcome to Quiz Ninja!');
-const question = "What is Superman's real name?";
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+
 const quiz = [
     ["What is Superman's real name?","Clark Kent"],
     ["What is Wonder Woman's real name?", "Diana Prince"],
     ["What is Batman's real name?", "Bruce Wayne"]
 ];
-let score = 0; //start of the scoring;
-for(const [question,answer] of quiz){
-    const response = prompt(question);
-    if(response == answer){
-        alert('Correct!');
-        score++;
-    } else {
-        alert (`Wrong! The correct answer was ${answer}`);
+function start(quiz){
+    let score = 0;
+
+    //main game loop
+    for(const [question, answer] of quiz) {
+        const response = ask(question);
+        check (response,answer);
+    }
+    //end main game loop
+
+    gameOver();
+
+    //function declarations
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(response, answer) {
+        if (response === answer) {
+            alert(`Correct`);
+            score ++;
+        } else {
+            alert (`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver() {
+        alert (`Game Over, you scored ${score} points ${score !== 1 ? 's': ''}`);
     }
 }
-alert(`Game Over, You scored ${score} point${score !==1 ? 's' : ''}`);
+
+start(quiz);
