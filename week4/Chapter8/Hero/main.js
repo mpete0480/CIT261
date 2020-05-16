88bbb89fd981b4de7da34f55cecb782a54c44205
mpete0480 <mdpetersen1184@gmail.com>
2020-05-15 23:33:18 -0500

    hero update

--- week4/Chapter8/Hero/main.js
+++ week4/Chapter8/Hero/main.js
@@ -5,6 +5,7 @@ function makeHero(event) {
     event.preventDefault();
     const hero = {};
     hero.name = form.heroName.value;
+    hero.realName = form.realName.value;
     alert(JSON.stringify(hero));
     return hero;
 }
