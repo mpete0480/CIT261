da17359a47e1a23b4005940c82b8bc937aab6220
mpete0480 <mdpetersen1184@gmail.com>
2020-10-29 20:41:17 -0500

    updates for chapter 7

---
+++ week7/main.js
@@ -0,0 +1,20 @@
+const textButton = document.querySelector('#number');
+const apiButton = document.querySelector('#chuck');
+const outputDiv = document.querySelector('#output');
+
+const textURL = `http://numbersapi.com/random`;
+const apiURL = `https://api.chucknorris.io/jokes/random`;
+
+textButton.addEventListener('click',()=> {
+    fetch (textURL)
+    .then ( response => {
+        outputDiv.innerHTML = 'Waiting for response...';
+    if (response.ok) {
+        return response;
+    }
+        throw Error(response.statusText);
+    })
+    .then ( response => response.text() )
+    .then ( text => outputDiv.innerText = text )
+    .catch( error => console.log('There was an error:',error))
+},false);
